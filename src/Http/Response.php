27d414c3b5d319972fd722429ca60c1481e627db
Ayesh Karunaratne <ayesh@aye.sh>
2022-10-26 16:58:13 +0530

    Add HTTP Response stub

---
+++ src/Http/Response.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Absmartly\SDK\Http;
+
+class Response {
+	public int $status; // @todo Make read-only
+	public string $statusMessage; // @todo Make read-only
+	public string $contentType; // @todo Make read-only
+	public string $content; // @todo Make read-only
+}
